6b893d595d58d89b8d1fc7472eca9c675a7c05c4
salomon.pedro <salomon.pedro@fronius.com>
2020-04-06 10:18:40 +0200

    Bootstrap and material toolbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AdminRoutingModule } from './micro-apps/admin/admin-routing.module';
 import { RouterModule } from '@angular/router';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {  MatButtonModule } from '@angular/material/button'
 @NgModule({
   declarations: [
     AppComponent
@@ -17,6 +19,9 @@ import { RouterModule } from '@angular/router';
     AppRoutingModule,
     BrowserAnimationsModule,
     AdminRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/micro-apps/admin/admin.module.ts
+++ src/app/micro-apps/admin/admin.module.ts
@@ -4,11 +4,14 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './dashboard/admin-dashboard.component';
 import { AdminComponent } from './admin-apps/admin.component';
 
+
 @NgModule({
-    declarations:[AdminDashboardComponent, AdminComponent],
-    imports:[AdminRoutingModule, RouterModule],
-    exports:[RouterModule, AdminDashboardComponent]
+    declarations: [AdminDashboardComponent, AdminComponent],
+    imports: [AdminRoutingModule, RouterModule,
+      
+    ],
+    exports: [RouterModule, AdminDashboardComponent]
 })
-export class AdminModule{
+export class AdminModule {
 
 }
