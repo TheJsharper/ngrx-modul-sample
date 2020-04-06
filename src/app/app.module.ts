2d595f7bd5b9890837860df1b02672675b7c6d1d
salomon.pedro <salomon.pedro@fronius.com>
2020-04-06 11:42:35 +0200

    sidev bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {  MatButtonModule } from '@angular/material/button'
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
 @NgModule({
   declarations: [
     AppComponent
@@ -21,7 +24,10 @@ import {  MatButtonModule } from '@angular/material/button'
     AdminRoutingModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
