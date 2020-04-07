59e7dbea0bb9f2cf11ed2a7a9921ca9207aba684
salomon.pedro <salomon.pedro@fronius.com>
2020-04-07 16:05:47 +0200

    World population table added but buggy

--- src/app/micro-apps/world-population/table/world.population.tableView.component.ts
+++ src/app/micro-apps/world-population/table/world.population.tableView.component.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { Countries, Country, keys } from '../models/model.contries';
 
 @Component({
-    selector:'world-table-view',
-    templateUrl:'./world.population.tableView.component.html'
+    selector: 'world-table-view',
+    templateUrl: './world.population.tableView.component.html'
 })
-export class WorldPopulationTableViewComponent{
+export class WorldPopulationTableViewComponent {
 
+    dataSource: Country[] = Countries;
+    keys: String[];
+    config: HeaderRename[];
+    constructor() {
+        this.keys = keys;
+        this.config = this.getConfig();
+        //['name', 'position', 'weight', 'symbol', 'position', 'weight', 'symbol', 'star'];
+    }
+    getValue(element: any, key: string): any {
+        return element[key];
+    }
+    private getConfig(): HeaderRename[] {
+        const result: HeaderRename[] = this.keys.reduce((prev: HeaderRename[], cur: string) => {
+            const newObject: HeaderRename = {
+                property: cur,
+                name: cur.includes("_") ? cur.split("_")[1] : cur
+            };
+            prev.push(newObject);
+            return prev;
+        }, []);
+        result.splice(5, 0, { property: "star", name: "" });
+        //result.push({property:"star", name:""})
+        console.log("--->", result);
+        return result;
+    }
+
+}
+
+export class HeaderRename {
+    property: string;
+    name: string;
 }
--- src/app/micro-apps/world-population/world.population.module.ts
+++ src/app/micro-apps/world-population/world.population.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from "@angular/core";
 import { WorldPopulationTableViewComponent } from './table/world.population.tableView.component';
 import { WorldPopulationRouting } from './world.population-routing.module';
 import { RouterModule } from '@angular/router';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from  "@angular/material/icon";
+import { from } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [WorldPopulationTableViewComponent],
-    imports: [WorldPopulationRouting, RouterModule],
+    imports: [WorldPopulationRouting, RouterModule, MatTableModule, MatIconModule, CommonModule],
     exports: [RouterModule, WorldPopulationTableViewComponent]
 })
 export class WorldPopulationModule { }
