e1d6188fbd6e8b367bc53373453add3baaa16193
salomon.pedro <salomon.pedro@fronius.com>
2020-04-08 13:11:20 +0200

    world population table is ready

--- src/app/micro-apps/world-population/table/world.population.tableView.component.ts
+++ src/app/micro-apps/world-population/table/world.population.tableView.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { Countries, keys } from '../models/model.contries';
+import { Countries, keys, Country } from '../models/model.contries';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
     selector: 'world-table-view',
     templateUrl: './world.population.tableView.component.html',
-    styleUrls:['./world.population.tabbleView.component.scss']
+    styleUrls: ['./world.population.tabbleView.component.scss']
 })
 export class WorldPopulationTableViewComponent implements OnInit {
 
     dataSource = new MatTableDataSource(Countries);
     keys: String[];
+    key
     config: HeaderRename[];
-    @ViewChild(MatSort, {static: true}) sort: MatSort;
-    @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+    @ViewChild(MatSort, { static: true }) sort: MatSort;
+    @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
     constructor() {
         this.keys = keys;
         this.keys.push("star");
@@ -43,7 +44,28 @@ export class WorldPopulationTableViewComponent implements OnInit {
     applyFilter(event: Event) {
         const filterValue = (event.target as HTMLInputElement).value;
         this.dataSource.filter = filterValue.trim().toLowerCase();
-      }
+    }
+
+    isYear(value: any): boolean {
+
+        if (!isNaN(value)) {
+            const n: number = parseInt(value);
+            return n >= 1960;
+        } else {
+            return false;
+        }
+    }
+    getTotal(value: string): number {
+        return Countries.reduce((prev: number, cur, ) => {
+
+            if (!isNaN(parseInt(cur[value])))
+                prev += parseInt(cur[value]);
+                else{
+                    console.log("--->",cur[value])
+                }
+            return prev;
+        }, 0)
+    }
 
 }
 
--- src/app/micro-apps/world-population/world.population.module.ts
+++ src/app/micro-apps/world-population/world.population.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from "@angular/material/icon";
 import { MatTableModule } from '@angular/material/table';
+import {CdkTableModule} from '@angular/cdk/table';
 import { RouterModule } from '@angular/router';
 import { WorldPopulationTableViewComponent } from './table/world.population.tableView.component';
 import { WorldPopulationRouting } from './world.population-routing.module';
@@ -13,7 +14,7 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 @NgModule({
     declarations: [WorldPopulationTableViewComponent],
     imports: [WorldPopulationRouting,
-         RouterModule, MatTableModule, MatIconModule, MatFormFieldModule,
+         RouterModule, MatTableModule, CdkTableModule, MatIconModule, MatFormFieldModule,
           MatInputModule, MatSortModule, 
           MatPaginatorModule,
           FormsModule, CommonModule],
