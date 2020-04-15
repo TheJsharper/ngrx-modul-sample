f537a569993bfee7caa23e109e408b2e0de4d5eb
salomon.pedro <salomon.pedro@fronius.com>
2020-04-15 14:19:13 +0200

    charts are ready it would improve styling

--- src/app/micro-apps/world-population-charts/charts/world.population.chartView.component.ts
+++ src/app/micro-apps/world-population-charts/charts/world.population.chartView.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, Renderer2, OnDestroy } from "@angular/core";
 import * as Chart from 'chart.js';
-import { ChartConfiguration } from 'chart.js';
+import { ChartConfiguration, ChartDataSets } from 'chart.js';
 
 @Component({
     selector: 'charts-view',
@@ -53,12 +53,12 @@ export class WorldPopulationChartViewComponent implements OnInit, OnDestroy {
     }
 
     private update(next: number): void {
-        if (next < 20) {
+        if (next < 2) {
             this.timeoutRef.push(setTimeout(() => {
                 window.requestAnimationFrame((f: number) => {
-                    console.log("---->", this.chart.data);
                     const colors: string[] = this.getBorderColors();
-                    const background:string[]= [`rgba(255, 255, 2555, 0.0)`]
+                    const background: string[] = [`rgba(255, 255, 2555, 0.0)`]
+
                     this.chart.data.datasets.push({ label: 'of votes ' + next, data: this.getRandomData(), borderColor: colors, backgroundColor: background, borderWidth: 5 });
                     next++;
                     this.chart.update();
@@ -66,26 +66,48 @@ export class WorldPopulationChartViewComponent implements OnInit, OnDestroy {
                 });
             }, 1000));
 
+        } else if (next >= 2 && next <= 100) {
+
+            this.timeoutRef.push(setTimeout(() => {
+                window.requestAnimationFrame((f: number) => {
+                    this.chart.data.datasets.forEach((val: ChartDataSets) => val.data.push(this.getNextData()));
+
+                    next++;
+                    this.chart.update();
+                    this.update(next);
+                });
+            }, 1000));
+
+
+
+
         }
 
     }
     private getRandomData(): number[] {
-        return Array(7).fill(0).map((_: number) => Math.abs(Math.floor(Math.random() * 20)))
+        return Array(2).fill(0).map((_: number) => Math.abs(Math.floor(Math.random() * 20)))
 
+    }
+    private getNextData(): number {
+        return Math.abs(Math.floor(Math.random() * 20));
     }
 
     private getBorderColors(): string[] {
-
 
         const next: number[] = Array(3).fill(0).map((_: number) => Math.abs(Math.floor(100 + Math.random() * (255 - 100))));
         return [`rgba(${next[0]}, ${next[1]}, ${next[2]}, 0.5)`];
     }
 
+    private getLabels(): string[] {
+        const fullYears: string[] = new Array(60).fill(new Date(1960, 1, 1)).map((date: Date, index: number) => date.getFullYear() + index).map((value: number) => value.toString());
+        return fullYears;
+
+    }
     private getDataCanvas(): ChartConfiguration {
         return {
             type: 'line',
             data: {
-                labels: ['Paraguay', 'Argentina', 'Brasil', 'Uruguay', 'Chile', 'Bolivia'],
+                labels: this.getLabels(),
                 datasets: []
             },
             options: {
@@ -95,6 +117,11 @@ export class WorldPopulationChartViewComponent implements OnInit, OnDestroy {
                             beginAtZero: true
                         }
                     }]
+                },
+                animation: {
+                    duration: 1500,
+                    animateRotate: true,
+                    easing: "linear"
                 }
             }
         };
